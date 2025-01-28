7cfedf11b47765cfad44adea326e75983d044faf
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-01-28 15:04:28 +0100

    Add FTP server client and main server socket implementation

---
+++ ftp-server/src/main/java/com/ftp/server/Client.java
@@ -0,0 +1,14 @@
+package com.ftp.server;
+
+import java.net.Socket;
+
+public class Client implements Runnable {
+    private Socket socket;
+
+    public Client(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+    }
+}
--- ftp-server/src/main/java/com/ftp/server/Main.java
+++ ftp-server/src/main/java/com/ftp/server/Main.java
@@ -1,13 +1,23 @@
 package com.ftp.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    private static ServerSocket server;
+    private static int port = 21;
+
+    public static void main(String[] args) throws IOException {
+        server = new ServerSocket(port);
+
+        while (true) {
+            Socket socket = server.accept();
+
+        }
     }
 }
