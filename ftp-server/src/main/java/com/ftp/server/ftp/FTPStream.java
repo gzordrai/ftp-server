36b55dc7f09dbafac69be4d4c7ad0d1346f7ae6f
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-01-28 16:39:33 +0100

    Add FTPStream class for handling input/output streams in FTP connections

---
+++ ftp-server/src/main/java/com/ftp/server/ftp/FTPStream.java
@@ -0,0 +1,29 @@
+package com.ftp.server.ftp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class FTPStream {
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public FTPStream(Socket socket) throws IOException {
+        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public String read() throws IOException {
+        return this.reader.readLine();
+    }
+
+    public void write(FTPResponseCode response) {
+        this.writer.println(response.toString());
+    }
+
+    public void write(String message) {
+        this.writer.println(message);
+    }
+}
