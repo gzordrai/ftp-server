f680e108dbe745079a32d0c7a15fc93568e05b48
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-02-03 10:30:26 +0100

    Update FTPServer to initialize Volume with root path and allow retrieval of Volume instance

--- ftp-server/src/main/java/com/ftp/server/ftp/FTPServer.java
+++ ftp-server/src/main/java/com/ftp/server/ftp/FTPServer.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.ftp.server.fs.Volume;
+
 public class FTPServer {
-    public static void start(int port) throws IOException {
+    private static Volume volume;
+
+    public static void start(int port, String rootPath) throws IOException {
+        volume = new Volume(rootPath);
         ServerSocket server = new ServerSocket(port);
 
         while (true) {
@@ -13,7 +18,11 @@ public class FTPServer {
             System.out.println("New connection from " + socket.getInetAddress().getHostAddress());
             FTPClient client = new FTPClient(socket);
 
-            client.run();
+            new Thread(client).start();
         }
+    }
+
+    public static Volume getVolume() {
+        return volume;
     }
 }
