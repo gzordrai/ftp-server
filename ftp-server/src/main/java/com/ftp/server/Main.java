404484e349b410dc20b7a17fb8684a46c797b5ab
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-02-03 10:09:22 +0100

    Update Main class to require volume path and validate its existence

--- ftp-server/src/main/java/com/ftp/server/Main.java
+++ ftp-server/src/main/java/com/ftp/server/Main.java
@@ -1,20 +1,26 @@
 package com.ftp.server;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import com.ftp.server.ftp.FTPServer;
 
-/**
- * Hello world!
- *
- */
 public class Main {
     public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
-            System.out.println("Usage: java -jar ftp-server.jar <port>");
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar ftp-server.jar <port> <volume>");
             System.exit(1);
         }
 
-        FTPServer.start(Integer.parseInt(args[0]));
+        int port = Integer.parseInt(args[0]);
+        String rootPath = args[1];
+
+        if (!Files.exists(Paths.get(rootPath))) {
+            System.out.println("Error: The specified volume path does not exist.");
+            System.exit(1);
+        }
+
+        FTPServer.start(port, rootPath);
     }
 }
