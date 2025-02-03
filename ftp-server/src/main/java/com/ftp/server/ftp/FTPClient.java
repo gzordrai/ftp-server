2cdcaf36b536b4d40b87223eff18b7cef4b8b984
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-02-03 10:36:17 +0100

    Implement PASV and LIST command handling in FTPClient for improved data connection management

--- ftp-server/src/main/java/com/ftp/server/ftp/FTPClient.java
+++ ftp-server/src/main/java/com/ftp/server/ftp/FTPClient.java
@@ -1,17 +1,23 @@
 package com.ftp.server.ftp;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.concurrent.CountDownLatch;
 
 public class FTPClient implements Runnable {
     private FTPStream commandStream;
     private FTPStream dataStream;
+    private ServerSocket dataServerSocket;
+    private CountDownLatch dataConnectionLatch;
 
     public FTPClient(Socket socket) throws IOException {
         this.commandStream = new FTPStream(socket);
     }
 
-    private void processCommand(String command) {
+    private void processCommand(String command) throws IOException, InterruptedException {
         try {
             String[] commandParts = command.split(" ");
             FTPCommand ftpCommand = FTPCommand.valueOf(commandParts[0].toUpperCase());
@@ -49,11 +55,10 @@ public class FTPClient implements Runnable {
                     break;
 
                 case PASV:
-                    this.commandStream.write(FTPResponseCode.ENTERING_PASSIVE_MODE.getMessage("127,0,0,1,0,0"));
+                    handlePASV();
                     break;
-
                 case LIST:
-                    System.out.println("List command");
+                    handleList();
                     break;
                 case CWD:
                     System.out.println("Cwd command");
@@ -71,8 +76,59 @@ public class FTPClient implements Runnable {
                     System.out.println("Invalid command: " + command);
             }
         } catch (IllegalArgumentException e) {
-            System.out.println("Invalid commandO: " + command);
+            System.out.println("Invalid command: " + command);
         }
+    }
+
+    private void handlePASV() throws IOException {
+        this.dataConnectionLatch = new CountDownLatch(1);
+
+        if (dataServerSocket != null && !dataServerSocket.isClosed()) {
+            dataServerSocket.close();
+        }
+
+        dataServerSocket = new ServerSocket(0); // Bind to any available port
+        int port = dataServerSocket.getLocalPort();
+        String hostAddress = commandStream.getSocket().getLocalAddress().getHostAddress().replace('.', ',');
+
+        String response = String.format("227 Entering Passive Mode (%s,%d,%d)", hostAddress, port / 256, port % 256);
+        this.commandStream.write(response);
+
+        try {
+            Socket dataSocket = dataServerSocket.accept();
+            this.dataStream = new FTPStream(dataSocket);
+            this.dataConnectionLatch.countDown();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleList() throws IOException, InterruptedException {
+        this.commandStream.write(FTPResponseCode.TRANSFER_START);
+        StringBuilder files = new StringBuilder();
+
+        for (File f : FTPServer.getVolume().listFiles()) {
+            if (f.isDirectory()) {
+                files.append("drwxr-xr-x 1 owner group ").append(f.length()).append(" Jan 1 00:00 ")
+                        .append(f.getName()).append("\r\n");
+            } else {
+                files.append("-rw-r--r-- 1 owner group ").append(f.length()).append(" Jan 1 00:00 ")
+                        .append(f.getName()).append("\r\n");
+            }
+        }
+
+        if (dataConnectionLatch != null)
+            dataConnectionLatch.await();
+
+        if (dataStream != null) {
+            this.dataStream.write(files.toString());
+            this.dataStream.close();
+            this.dataStream = null;
+        } else {
+            this.commandStream.write("425 Can't open data connection.");
+        }
+
+        this.commandStream.write(FTPResponseCode.DIRECTORY_SENDED);
     }
 
     public void run() {
@@ -81,8 +137,15 @@ public class FTPClient implements Runnable {
 
             this.commandStream.write(FTPResponseCode.WELCOME);
 
-            while ((command = this.dataStream.read()) != null) {
+            while ((command = this.commandStream.read()) != null) {
                 this.processCommand(command);
+            }
+        } catch (SocketException e) {
+            try {
+                this.commandStream.close();
+                this.dataStream.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
             }
         } catch (Exception e) {
             e.printStackTrace();
