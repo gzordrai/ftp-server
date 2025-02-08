3c7dd7c13c68e84168f401358318b81ac5cf7ef1
gzordrai <gzordrai@users.noreply.github.com>
2025-02-08 17:42:44 +0100

    Implement logging for new connections and add server shutdown mechanism

--- src/main/java/com/ftp/server/ftp/FTPServer.java
+++ src/main/java/com/ftp/server/ftp/FTPServer.java
@@ -3,22 +3,39 @@ package com.ftp.server.ftp;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.ftp.server.fs.Volume;
 
 public class FTPServer {
     private static Volume volume;
+    private static final Logger logger = LogManager.getLogger(FTPServer.class);
+
+    private FTPServer() {
+    }
 
     public static void start(int port, String rootPath) throws IOException {
         volume = new Volume(rootPath);
-        ServerSocket server = new ServerSocket(port);
 
-        while (true) {
-            Socket socket = server.accept();
-            System.out.println("New connection from " + socket.getInetAddress().getHostAddress());
-            FTPClient client = new FTPClient(socket);
+        try (ServerSocket server = new ServerSocket(port)) {
+            boolean running = true;
 
-            new Thread(client).start();
+            while (running) {
+                Socket socket = server.accept();
+                logger.info("New connection from {}", socket.getInetAddress().getHostAddress());
+                FTPClient client = new FTPClient(socket);
+
+                new Thread(client).start();
+
+                if (Files.exists(Paths.get(rootPath, "stop.txt"))) {
+                    logger.info("Stop signal received. Shutting down the server.");
+                    running = false;
+                }
+            }
         }
     }
 
