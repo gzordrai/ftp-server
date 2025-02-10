70904e2b62ad338abcb555182ddb403c36946033
gzordrai <gzordrai@users.noreply.github.com>
2025-02-10 17:41:06 +0100

    Update pwd command and add javadoc

--- src/main/java/com/ftp/server/ftp/commands/PwdCommand.java
+++ src/main/java/com/ftp/server/ftp/commands/PwdCommand.java
@@ -3,17 +3,29 @@ package com.ftp.server.ftp.commands;
 import com.ftp.server.ftp.FTPClient;
 import com.ftp.server.ftp.FTPResponseCode;
 
+/**
+ * This class implements the PWD (Print Working Directory) command.
+ * It sends the current directory of the FTP client.
+ */
 public class PwdCommand implements Command {
     private final FTPClient client;
 
+    /**
+     * Constructs a new PwdCommand.
+     *
+     * @param client the FTP client
+     */
     public PwdCommand(FTPClient client) {
         this.client = client;
     }
 
+    /**
+     * Executes the PWD command.
+     * Sends the current directory of the FTP client.
+     */
     @Override
     public void execute() {
-        // To change
-        String currentDirectory = "/";
-        client.sendResponse(FTPResponseCode.CURRENT_DIRECTORY.getMessage(currentDirectory));
+        this.client.sendResponse(
+                FTPResponseCode.CURRENT_DIRECTORY.getMessage(this.client.getVolume().getCurrentDirectory()));
     }
 }
