b8603afe6a52f4b34f95003f68eadc1b1baf6134
gzordrai <gzordrai@users.noreply.github.com>
2025-02-10 17:41:57 +0100

    Add javadoc

--- src/main/java/com/ftp/server/ftp/commands/QuitCommand.java
+++ src/main/java/com/ftp/server/ftp/commands/QuitCommand.java
@@ -3,13 +3,26 @@ package com.ftp.server.ftp.commands;
 import com.ftp.server.ftp.FTPClient;
 import com.ftp.server.ftp.FTPResponseCode;
 
+/**
+ * This class implements the QUIT command.
+ * It sends a goodbye message to the client and closes the connection.
+ */
 public class QuitCommand implements Command {
     private final FTPClient client;
 
+    /**
+     * Constructs a new QuitCommand.
+     *
+     * @param client the FTP client
+     */
     public QuitCommand(FTPClient client) {
         this.client = client;
     }
 
+    /**
+     * Executes the QUIT command.
+     * Sends a goodbye message to the client and closes the connection.
+     */
     @Override
     public void execute() {
         this.client.sendResponse(FTPResponseCode.GOODBYE.toString());
