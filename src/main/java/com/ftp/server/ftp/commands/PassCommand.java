a3788b9014fd28b1b8e922e370a1236fcd55ef69
gzordrai <gzordrai@users.noreply.github.com>
2025-02-10 17:57:28 +0100

    Add javadoc

--- src/main/java/com/ftp/server/ftp/commands/PassCommand.java
+++ src/main/java/com/ftp/server/ftp/commands/PassCommand.java
@@ -3,23 +3,36 @@ package com.ftp.server.ftp.commands;
 import com.ftp.server.ftp.FTPClient;
 import com.ftp.server.ftp.FTPResponseCode;
 
+/**
+ * This class implements the PASS command.
+ * It handles the password input from the client.
+ */
 public class PassCommand implements Command {
     private final FTPClient client;
     private final String[] args;
 
+    /**
+     * Constructs a new PassCommand.
+     *
+     * @param client the FTP client
+     * @param args the command arguments
+     */
     public PassCommand(FTPClient client, String[] args) {
         this.client = client;
         this.args = args;
     }
 
+    /**
+     * Executes the PASS command.
+     * Handles the password input from the client.
+     */
     @Override
     public void execute() {
         if (args.length > 1) {
             String password = args[1];
 
-            client.sendResponse(FTPResponseCode.LOGIN_SUCCESSFUL.toString());
-        } else {
-            client.sendResponse(FTPResponseCode.SYNTAX_ERROR.toString());
-        }
+            this.client.sendResponse(FTPResponseCode.LOGIN_SUCCESSFUL.toString());
+        } else
+            this.client.sendResponse(FTPResponseCode.SYNTAX_ERROR.toString());
     }
 }
