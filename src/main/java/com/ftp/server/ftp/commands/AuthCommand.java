896e4d2c432d9f98f1f76ac8ca23e339e1d269ed
gzordrai <gzordrai@users.noreply.github.com>
2025-02-10 17:47:11 +0100

    Add javadoc

--- src/main/java/com/ftp/server/ftp/commands/AuthCommand.java
+++ src/main/java/com/ftp/server/ftp/commands/AuthCommand.java
@@ -3,20 +3,34 @@ package com.ftp.server.ftp.commands;
 import com.ftp.server.ftp.FTPClient;
 import com.ftp.server.ftp.FTPResponseCode;
 
+/**
+ * This class implements the AUTH command.
+ * It handles authentication mechanisms such as SSL.
+ */
 public class AuthCommand implements Command {
     private final FTPClient client;
     private final String[] args;
 
+    /**
+     * Constructs a new AuthCommand.
+     *
+     * @param client the FTP client
+     * @param args the command arguments
+     */
     public AuthCommand(FTPClient client, String[] args) {
         this.client = client;
         this.args = args;
     }
 
+    /**
+     * Executes the AUTH command.
+     * Handles authentication mechanisms such as SSL.
+     */
     @Override
     public void execute() {
         if (args.length > 1 && "SSL".equalsIgnoreCase(args[1]))
-            client.sendResponse(FTPResponseCode.LOGIN_WITH_USER_PASS.toString());// client.sendResponse("234 AUTH SSL successful");
+            this.client.sendResponse(FTPResponseCode.LOGIN_WITH_USER_PASS.toString()); // this.client.sendResponse("234 AUTH SSL successful");
         else
-            client.sendResponse(FTPResponseCode.LOGIN_WITH_USER_PASS.toString());
+            this.client.sendResponse(FTPResponseCode.LOGIN_WITH_USER_PASS.toString());
     }
 }
