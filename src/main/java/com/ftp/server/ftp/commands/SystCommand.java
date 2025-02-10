5ad50882dbb5e4aee5b78345fee498c4daacf340
gzordrai <gzordrai@users.noreply.github.com>
2025-02-10 18:11:02 +0100

    Refactor SystCommand to dynamically send system type and add Javadoc comments

--- src/main/java/com/ftp/server/ftp/commands/SystCommand.java
+++ src/main/java/com/ftp/server/ftp/commands/SystCommand.java
@@ -1,16 +1,32 @@
 package com.ftp.server.ftp.commands;
 
 import com.ftp.server.ftp.FTPClient;
+import com.ftp.server.ftp.FTPResponseCode;
 
+/**
+ * This class implements the SYST command.
+ * It sends the system type and a list of all available commands to the client.
+ */
 public class SystCommand implements Command {
     private final FTPClient client;
 
+    /**
+     * Constructs a new SystCommand.
+     *
+     * @param client the FTP client
+     */
     public SystCommand(FTPClient client) {
         this.client = client;
     }
 
+    /**
+     * Executes the SYST command.
+     * Sends the system type.
+     */
     @Override
     public void execute() {
-        client.sendResponse("215 UNIX Type: L8");
+        String systemType = System.getProperty("os.name");
+
+        this.client.sendResponse(FTPResponseCode.SYSTEM_TYPE.getMessage(systemType));
     }
 }
