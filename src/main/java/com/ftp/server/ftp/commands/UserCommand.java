d1f9b6a8fc72205a4a3ba12b7190095512781c41
gzordrai <gzordrai@users.noreply.github.com>
2025-02-10 17:56:12 +0100

    Add javadoc

--- src/main/java/com/ftp/server/ftp/commands/UserCommand.java
+++ src/main/java/com/ftp/server/ftp/commands/UserCommand.java
@@ -3,23 +3,36 @@ package com.ftp.server.ftp.commands;
 import com.ftp.server.ftp.FTPClient;
 import com.ftp.server.ftp.FTPResponseCode;
 
+/**
+ * This class implements the USER command.
+ * It handles the username input from the client.
+ */
 public class UserCommand implements Command {
     private final FTPClient client;
     private final String[] args;
 
+    /**
+     * Constructs a new UserCommand.
+     *
+     * @param client the FTP client
+     * @param args the command arguments
+     */
     public UserCommand(FTPClient client, String[] args) {
         this.client = client;
         this.args = args;
     }
 
+    /**
+     * Executes the USER command.
+     * Handles the username input from the client.
+     */
     @Override
     public void execute() {
         if (args.length > 1) {
             String username = args[1];
 
-            client.sendResponse(FTPResponseCode.SPECIFY_PASSWORD.toString());
-        } else {
-            client.sendResponse(FTPResponseCode.SYNTAX_ERROR.toString());
-        }
+            this.client.sendResponse(FTPResponseCode.SPECIFY_PASSWORD.toString());
+        } else
+            this.client.sendResponse(FTPResponseCode.SYNTAX_ERROR.toString());
     }
 }
