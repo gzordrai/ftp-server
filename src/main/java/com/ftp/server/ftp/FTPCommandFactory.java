4816a0b8c9c5d81119bfea1fe8ddb473cfca3255
gzordrai <gzordrai@users.noreply.github.com>
2025-02-10 18:12:09 +0100

    Add FEAT command implementation and enhance logging in FTPCommandFactory

--- src/main/java/com/ftp/server/ftp/FTPCommandFactory.java
+++ src/main/java/com/ftp/server/ftp/FTPCommandFactory.java
@@ -4,9 +4,14 @@ import com.ftp.server.ftp.commands.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FTPCommandFactory {
     private static final Map<String, Class<? extends Command>> commandHandlers = new HashMap<>();
+    private static final Logger logger = LogManager.getLogger(FTPCommandFactory.class);
 
     private FTPCommandFactory() {
     }
@@ -16,21 +21,23 @@ public class FTPCommandFactory {
         commandHandlers.put("USER", UserCommand.class);
         commandHandlers.put("PASS", PassCommand.class);
         commandHandlers.put("SYST", SystCommand.class);
+        commandHandlers.put("FEAT", FeatCommand.class);
         commandHandlers.put("PWD", PwdCommand.class);
         commandHandlers.put("TYPE", TypeCommand.class);
         commandHandlers.put("PASV", PasvCommand.class);
         commandHandlers.put("EPSV", EpsvCommand.class);
         commandHandlers.put("LIST", ListCommand.class);
         commandHandlers.put("CWD", CwdCommand.class);
+        // commandHandlers.put("CDUP", CdupCommand.class);
         // commandHandlers.put("RETR", RetrCommand.class);
         // commandHandlers.put("STOR", StorCommand.class);
-        // commandHandlers.put("QUIT", QuitCommand.class);
+        commandHandlers.put("QUIT", QuitCommand.class);
     }
 
     public static Command getHandler(String command, FTPClient client, String[] args) {
         Class<? extends Command> handlerClass = commandHandlers.get(command.toUpperCase());
 
-        System.out.println(command + args.length);
+        logger.info("Command: {} from client: {}", command, "");
 
         if (handlerClass != null) {
             try {
@@ -44,5 +51,9 @@ public class FTPCommandFactory {
         }
 
         return null;
+    }
+
+    public static Set<String> getCommandHandlers() {
+        return commandHandlers.keySet();
     }
 }
---
+++ src/main/java/com/ftp/server/ftp/commands/FeatCommand.java
@@ -0,0 +1,38 @@
+package com.ftp.server.ftp.commands;
+
+import com.ftp.server.ftp.FTPClient;
+import com.ftp.server.ftp.FTPCommandFactory;
+
+import java.util.Set;
+
+/**
+ * This class implements the FEAT command.
+ * It sends a list of all available features (commands) to the client.
+ */
+public class FeatCommand implements Command {
+    private final FTPClient client;
+
+    /**
+     * Constructs a new FeatCommand.
+     *
+     * @param client the FTP client
+     */
+    public FeatCommand(FTPClient client) {
+        this.client = client;
+    }
+
+    /**
+     * Executes the FEAT command.
+     * Sends a list of all available features (commands) to the client.
+     */
+    @Override
+    public void execute() {
+        StringBuilder response = new StringBuilder("211-Features:\n");
+        Set<String> commands = FTPCommandFactory.getCommandHandlers();
+
+        for (String command : commands)
+            response.append(" ").append(command).append("\n");
+
+        this.client.sendResponse(response.toString());
+    }
+}
